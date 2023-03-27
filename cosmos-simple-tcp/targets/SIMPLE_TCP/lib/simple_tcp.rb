4225db7c18ebc4aabfb8d20ebd0f6b0d000316f5
Fran Acien <amil101ftw@gmail.com>
2023-03-27 15:14:17 +0200

    update the readme

---
+++ cosmos-simple-tcp/targets/SIMPLE_TCP/lib/simple_tcp.rb
@@ -0,0 +1,10 @@
+# This class can be used in your scripts like so:
+#   require 'simple_tcp.rb'
+#   simple_tcp = SimpleTcp.new
+#   simple_tcp.utility
+# For more information see the COSMOS scripting guide
+
+class SimpleTcp
+  def utility
+  end
+end
